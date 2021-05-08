a52af1cd553e9fca1d07259d91cda94def2fdf54
Devilox <devilox96@yandex.ru>
2021-05-08 22:54:35 +0300

    Cleaned up a bit

--- ShallowWaterNew/Libs/dMath/Core/dVectors.h
+++
@@ -1,9 +0,0 @@
-#ifndef DVECTORS_H
-#define DVECTORS_H
-//----------------------------------//
-#include "dVector2D.h"
-#include "dVector3D.h"
-#include "dVectorND.h"
-#include "dQuaternion.h"
-//----------------------------------//
-#endif
